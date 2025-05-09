2d74857790b43037b1ce2f656bdd6551c1f26207
ChawinOph <oph.chawin@gmail.com>
2025-05-09 09:35:41 -0400

    updated cv

--- scripts/update_cv_json.sh
+++ scripts/update_cv_json.sh
@@ -8,7 +8,8 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # Define file paths
 CV_MARKDOWN="$BASE_DIR/_pages/cv.md"
-CV_JSON="$BASE_DIR/_data/cv.json"
+# CV_JSON="$BASE_DIR/_data/cv.json"
+CV_JSON="$BASE_DIR/_data/cv_chawin_ophaswongse.json"
 CONFIG_FILE="$BASE_DIR/_config.yml"
 
 # Check if the Python script exists
